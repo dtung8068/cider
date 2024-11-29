92a842a2423a098e72e8a700323cc80e747c7fcc
Scr0ll0 <56087882+Scr0ll0@users.noreply.github.com>
2024-11-29 17:01:59 -0500

    Added compatibility for numpy >= 1.2.0

--- utils/svhn_loader.py
+++ utils/svhn_loader.py
@@ -88,7 +88,7 @@ class SVHN(data.Dataset):
         if self.target_transform is not None:
             target = self.target_transform(target)
 
-        if np.lib.NumpyVersion(np.__version__) >= 1.2:
+        if np.lib.NumpyVersion(np.__version__) >= '1.2.0':
             return img, target.astype(np.longlong)
         return img, target.astype(np.long)
 
